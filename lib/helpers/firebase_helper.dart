af75d73aef670c28e6cadd1256a3899f6ec466a7
Aliryo Basyori <105861803+Aliryo@users.noreply.github.com>
2025-01-17 22:43:34 +0700

    add order

--- lib/helpers/firebase_helper.dart
+++ lib/helpers/firebase_helper.dart
@@ -103,8 +103,10 @@ class FirebaseHelper {
 
   //? Mengambil Data-Data Modul Dari Firebase
   static Future<List<ModuleMdl>> fetchModules() async {
-    final QuerySnapshot snapshot =
-        await FirebaseFirestore.instance.collection('modules').get();
+    final QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('modules')
+        .orderBy('moduleName', descending: false)
+        .get();
 
     List<ModuleMdl> modules = snapshot.docs
         .map((doc) =>
@@ -118,6 +120,7 @@ class FirebaseHelper {
   static Future<List<PartMdl>> fetchParts(String moduleName) async {
     final QuerySnapshot snapshot = await FirebaseFirestore.instance
         .collection('parts')
+        .orderBy('partName', descending: false)
         .where('moduleName', isEqualTo: moduleName)
         .get();
 
